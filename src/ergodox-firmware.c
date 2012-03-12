6570e7ae39948ac4db23bd995af7a276440cfed1
Ben Blazak <benblazak@gmail.com>
2012-03-11 18:52:31 -0700

    added src files - not much there yet though

---
+++ src/ergodox-firmware.c
@@ -0,0 +1,13 @@
+/* ----------------------------------------------------------------------------
+ * Firmware for the ergoDOX keyboard
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2012 Ben Blazak
+ * Released under The MIT License (MIT) (see "license.md") at
+ * <https://github.com/benblazak/ergodox-firmware>
+ * ------------------------------------------------------------------------- */
+
+// todo: set clock prescaler
+
+void main() {
+}
+
