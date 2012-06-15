d1f17133c92c7cfa390fb1f2b8e2ca2f493b4586
Ben Blazak <benblazak.dev@gmail.com>
2012-06-14 22:03:47 -0700

    added lib/key-functions private header

---
+++ src/lib/key-functions--private.h
@@ -0,0 +1,23 @@
+/* ----------------------------------------------------------------------------
+ * key functions: private
+ *
+ * Things to be used only by keyfunctions.  Exported any layouts would like to
+ * use these functions to help define their own.
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2012 Ben Blazak <benblazak.dev@gmail.com>
+ * Released under The MIT License (MIT) (see "license.md")
+ * Project located at <https://github.com/benblazak/ergodox-firmware>
+ * ------------------------------------------------------------------------- */
+
+
+#ifndef KEY_FUNCTIONS_h_PRIVATE
+	#define KEY_FUNCTIONS_h_PRIVATE
+
+	void _press_release(uint8_t keycode, bool pressed);
+	void _layer_set_current(
+			uint8_t value,
+			uint8_t * current_layer,
+			uint8_t * current_layers_[KB_ROWS][KB_COLUMNS] );
+
+#endif
+
