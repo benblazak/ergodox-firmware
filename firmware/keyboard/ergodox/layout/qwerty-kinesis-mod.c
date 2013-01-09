9bb6198878bd296c9703f871208d957c7932cecc
Ben Blazak <benblazak.dev@gmail.com>
2013-01-09 02:29:30 -0800

    intermediate checkin

--- firmware/keyboard/ergodox/layout/qwerty-kinesis-mod.c
+++ firmware/keyboard/ergodox/layout/qwerty-kinesis-mod.c
@@ -24,6 +24,7 @@
 
 #define  KEY_T  const kb__key_t * const PROGMEM
 
+// needed by ".../lib/layout/keys.h"
 #define  KEYS__DEFAULT(name, value)             \
         KEY_T name = {                          \
             .press_function   = &kf__press,     \
@@ -32,6 +33,7 @@
             .release_value    = value,          \
         }
 
+// needed by ".../lib/layout/keys.h"
 #define  KEYS__SHIFTED(name, value)                     \
         const uint16_t PROGMEM name##__press[] = { 2,   \
             &kf__press, KEY__LeftShift,                 \
@@ -51,20 +53,30 @@
 #include "../../../../firmware/lib/layout/keys.h"
 
 // special meaning
-KEY_T Transp   = NULL;                  // transparent
+KEY_T Transp   = NULL;  // transparent
 KEY_T NA       = { NULL, 0, NULL, 0 };  // do nothing
-// special function
+// special keycode
 KEYS__DEFAULT( Power,    KEY__Power      );
 KEYS__DEFAULT( VolumeU,  KEY__VolumeUp   );
 KEYS__DEFAULT( VolumeD,  KEY__VolumeDown );
 KEYS__DEFAULT( Mute,     KEY__Mute       );
+// special function
+#define  TWO_KEYS_CAPSLOCK  kf__two_keys_capslock_press_release
+KEY_T Sh2KCapL = { &TWO_KEYS_CAPSLOCK, (1<<8)|KEY__LeftShift,
+                   &TWO_KEYS_CAPSLOCK, (0<<8)|KEY__LeftShift };
+KEY_T Sh2KCapR = { &TWO_KEYS_CAPSLOCK, (1<<8)|KEY__RightShift,
+                   &TWO_KEYS_CAPSLOCK, (0<<8)|KEY__RightShift };
+#undef  TWO_KEYS_CAPSLOCK
+KEY_T Btldr    = { &kf__jump_to_bootloader, 0, NULL, 0 };
+// TODO
+KEY_T NumPush  = { &kf__layer__push, 0x23, NULL,              0 };
+KEY_T NumPop   = { &kf__layer__pop,  0x2,  NULL,              0 };
+// /TODO
 // layer
 KEY_T L0pu1    = { &kf__layer__push, 0x01, NULL,              0 };
 KEY_T L0po     = { &kf__layer__pop,  0x0,  NULL,              0 };
 KEY_T L0pu1po  = { &kf__layer__push, 0x01, &kf__layer__pop, 0x0 };
 KEY_T L1pu2po  = { &kf__layer__push, 0x12, &kf__layer__pop, 0x1 };
-KEY_T L2pu3    = { &kf__layer__push, 0x23, NULL,              0 };
-KEY_T L2po     = { &kf__layer__pop,  0x2,  NULL,              0 };
 
 
 // ----------------------------------------------------------------------------
@@ -78,16 +90,16 @@ NA,
    Equal,       K1,       K2,       K3,       K4,       K5,      Esc,
  Bkslash,        Q,        W,        E,        R,        T,    L0pu1,
      Tab,        A,        S,        D,        F,        G,
-  ShiftL,        Z,        X,        C,        V,        B,  L0pu1po,
+Sh2KCapL,        Z,        X,        C,        V,        B,  L0pu1po,
     GUIL,    Grave,  Bkslash,   ArrowL,   ArrowR,
                                                                CtrlL,     AltL,
                                                         NA,       NA,     Home,
                                                         Bs,      Del,      End,
 // right hand ..... ......... ......... ......... ......... ......... .........
-             L2pu3,       K6,       K7,       K8,       K9,       K0,     Dash,
+           NumPush,       K6,       K7,       K8,       K9,       K0,     Dash,
              BrktL,        Y,        U,        I,        O,        P,    BrktR,
                            H,        J,        K,        L,  Semicol,    Slash,
-           L0pu1po,        N,        M,    Comma,   Period,    Slash,   ShiftR,
+           L0pu1po,        N,        M,    Comma,   Period,    Slash, Sh2KCapR,
                                 ArrowL,   ArrowD,   ArrowU,   ArrowR,     GUIR,
     AltR,    CtrlR,
    PageU,  Nothing,       NA,
