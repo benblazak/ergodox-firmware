f3635dc57912dc9426350cc75c95f6a1edbd07b0
Ben Blazak <benblazak.dev@gmail.com>
2014-05-31 22:49:28 -0700

    got it to draw the keyboard :) really slow

--- layout-gen/main.elm
+++ layout-gen/main.elm
@@ -1,52 +1,139 @@
+-------------------------------------------------------------------------------
+-- Copyright (c) 2014 Ben Blazak <benblazak.dev@gmail.com> 
+-- Released under The MIT License (see "doc/licenses/MIT.md")
+-- Project located at <https://github.com/benblazak/ergodox-firmware>
+-------------------------------------------------------------------------------
+
+{-| A web-based UI for generating keyboard layouts for the firmware
+-}
+
+
 import Window
+import Dict
+
+import Boards
 
 
--- our grid's aspect ratio
-aspectRatio : (Int, Int)
-aspectRatio = (3,2)
--- to scale our aspect ratio
-aspectScale : Int
-aspectScale = 7
+-------------------------------------------------------------------------------
+-- values and signals
+-------------------------------------------------------------------------------
 
--- the number of columns in our grid
-columns : Int
-columns = aspectScale * (fst aspectRatio)
--- the number of rows in our grid
-rows : Int
-rows = aspectScale * (snd aspectRatio)
+{-| The number of columns and rows in our grid
 
--- to generate various transparencies of lightBlue
-lightBlueN : Float -> Color
-lightBlueN n = rgba 114 159 207 (1/n)
+Notes:
+- The list gives the aspect ratio (of width to height)
+- The number gives the scale of the aspect ratio, relative to the window size
+-}
+[columns, rows] = map ((*) 5) [4,3]
 
 
--- TODO: document what i mean by (x,y) (after i figure it out)
-background : (Int, Int) -> Element
-background (width,height) =
+{-| The edge length of a square in our grid
+-}
+edgeSignal : Signal Float
+edgeSignal =
+  let function = \(width,height) -> min (toFloat width / columns)
+                                        (toFloat height / rows)
+  in lift function Window.dimensions
+
+
+-------------------------------------------------------------------------------
+-- groups of forms
+-------------------------------------------------------------------------------
+
+background : (Int, Int) -> Float -> [Form]
+background (width,height) edge =
   let (w,h) = (toFloat width, toFloat height)
-      (r,c) = (toFloat rows, toFloat columns)
 
-      step = min (w/c) (h/r)
+      ruleLines = True
+      lineStyle = dashed lightBlue
+      ruleLineStyle = dotted charcoal
 
-      line (x,y) color offset =
-        segment (-x*w,-y*h) (x*w,y*h) |> traced (dashed color)
-                                      |> move (y*offset*step, x*offset*step)
+      -- x and y represent the horizontal and vertical scaling factors
+      line (x,y) style offset =
+        segment (-x*w/2,-y*h/2) (x*w/2,y*h/2)
+                |> traced style
+                |> move (y*offset*edge, x*offset*edge)
+
       lines (x,y) =
         let dimension = if x == 0 then w else h
-        in map (\n -> line (x/2,y/2)
-                           (lightBlueN <| toFloat (mod (n+1) 2 + 1) * 3)
-                           (toFloat n))
-               [floor (-dimension/step) .. floor (dimension/step)]
-      crossBox = [ line (0,1) charcoal 0,      line (1,0) charcoal 0, 
-                   line (0,1) charcoal ( c/2), line (0,1) charcoal (-c/2),
-                   line (1,0) charcoal ( r/2), line (1,0) charcoal (-r/2) ]
+        in map (\n -> line (x,y) lineStyle (toFloat n/2)
+                      |> alpha (1 / (toFloat (mod (n+1) 2 + 1) * 3)))
+               [floor (-dimension/edge)*2 .. floor (dimension/edge)*2]
 
-  in color lightGray <| collage width height (
-       crossBox ++
-       lines (0,1) ++ lines (1,0)
-     )
+      vLines = lines (0,1)
+      hLines = lines (1,0)
+
+      rLines =
+        let boxLine (x,y) offset = line (x,y) ruleLineStyle offset
+        in [ boxLine (0,1) (-columns/2), boxLine (1,0) (-rows/2)
+--            , boxLine (0,1) 0,            boxLine (1,0) 0
+           , boxLine (0,1) (columns/2),  boxLine (1,0) (rows/2)
+           ]
+
+  in hLines ++ vLines ++ if ruleLines then rLines else []
+
+
+keyboardTestDraw : Float -> [Form]
+keyboardTestDraw edge =
+  let lineStyle =
+        let temp = solid lightBlue
+        in { temp | width <- 5, cap <- Round, join <- Smooth }
+
+      -- grab just 1 Board, and use only 1 configuration
+      keyboard = head <| filter (\n -> n.name == "ErgoDox") Boards.all
+      configuration = 1
+
+      -- s = scaling factor (from 1 square of the grid)
+      -- p = position of the center of the rectangle on the grid
+      formKey k =
+        let (sizeX, sizeY) = (k.size*edge, edge)
+            (posX, posY) = ((fst k.position)*edge, (snd k.position)*edge)
+        in map (move (posX, posY) . rotate (degrees k.rotation))
+               [ rect sizeX sizeY |> outlined lineStyle
+               , toForm <| plainText k.name
+               ]
+
+      -- convert positions to be relative to center of the page
+      keys =
+        let convertPosition (x,y) = (  x - (fst keyboard.size) / 2,
+                                      -y + (snd keyboard.size) / 2 )
+        in map (\n -> { n | position <- convertPosition n.position })
+               keyboard.keys
+           |> filter ( \n ->    n.configuration == 0
+                             || n.configuration == configuration )
+
+  in foldl1 (++) (map formKey keys)
+
+--       leftHand =    (map (\n -> ((1.5,1),(-8.25,8-n)))[0..3])     -- left 1.5x
+--                  ++ (map (\n -> ((1,1),(-7+n,8)))[0..5])          -- top  1x
+--                  ++ (map (\n -> ((1,1),(-7+n,7)))[0..4])          -- next 1x
+--                  ++ (map (\n -> ((1,1),(-7+n,6)))[0..4])          -- next 1x
+--                  ++ (map (\n -> ((1,1),(-7+n,5)))[0..4])          -- next 1x
+--                  ++ (map (\n -> ((1,1),(-8+n,4)))[0..4])          -- bottom 1x
+--                  ++ (map (\n -> ((1,1.5),(-2,6.75-n*1.5)))[0..1]) -- right 1.5x
+--                  ++ (map (\n -> ((1,2),(-3+n,1.5)))[0..1])        -- thumb 2x
+--                  ++ (map (\n -> ((1,1),(-2+n,3)))[0..1])          -- t thumb 1x
+--                  ++ (map (\n -> ((1,1),(-1-n,2)))[0..0])          -- m thumb 1x
+--                  ++ (map (\n -> ((1,1),(-1-n,1)))[0..0])          -- b thumb 1x
+-- 
+--       rightHand = map (\((a,b),(c,d)) -> ((a,b),(-c,d))) leftHand
+-- 
+--   in map (\((a,b),(c,d)) -> key (a,b) (c-0.5,d-3)) (leftHand ++ rightHand)
+
+
+-------------------------------------------------------------------------------
+-- putting it all together
+-------------------------------------------------------------------------------
+
+scene : (Int, Int) -> Float -> Element
+scene (width,height) edge =
+  let backgroundColor = lightGray
+  in layers [ collage width height [] |> color backgroundColor
+            , collage width height (background (width,height) edge)
+            , collage width height (keyboardTestDraw edge)
+            ]
 
 
 main : Signal Element
-main = background <~ Window.dimensions
+main = scene <~ Window.dimensions ~ edgeSignal
 
---
+++ layout-gen/references.md
@@ -0,0 +1,27 @@
+## Elm Stuff
+
+* [Official Documentation]
+  (http://elm-lang.org)
+
+    * [Standard Libaries]
+      (http://library.elm-lang.org/catalog/elm-lang-Elm/0.12.3/)
+    * [Examples]
+      (http://elm-lang.org/Examples.elm)
+    * [Syntax: a Quick Tour]
+      (http://elm-lang.org/learn/Syntax.elm)
+    * [Types]
+      (http://elm-lang.org/learn/Getting-started-with-Types.elm)
+    * [Changes in Elm 0.10]
+      (http://elm-lang.org/blog/announce/0.10.elm)
+
+* [What does the “Just” syntax mean in Haskell?]
+  (http://stackoverflow.com/questions/18808258/what-does-the-just-syntax-mean-in-haskell/18809252#18809252)  
+  Haskell is very similar to Elm in some ways, so I think this applies.
+
+
+-------------------------------------------------------------------------------
+
+Copyright &copy; 2014 Ben Blazak <benblazak.dev@gmail.com>  
+Released under The MIT License (see "doc/licenses/MIT.md")  
+Project located at <https://github.com/benblazak/ergodox-firmware>
+
