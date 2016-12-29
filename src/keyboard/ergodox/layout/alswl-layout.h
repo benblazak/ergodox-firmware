f0a77d580d0fbcdfaf806bd88dc90e418f5b7921
alswl <alswlx@gmail.com>
2016-12-29 15:58:39 +0800

    feat: it works

---
+++ src/keyboard/ergodox/layout/alswl-layout.h
@@ -0,0 +1,30 @@
+/* ----------------------------------------------------------------------------
+ * ergoDOX : layout : DEFAULT : exports
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2012 Ben Blazak <benblazak.dev@gmail.com>
+ * Released under The MIT License (MIT) (see "license.md")
+ * Project located at <https://github.com/benblazak/ergodox-firmware>
+ * ------------------------------------------------------------------------- */
+
+
+#ifndef KEYBOARD__ERGODOX__LAYOUT__DEFAULT_h
+	#define KEYBOARD__ERGODOX__LAYOUT__DEFAULT_h
+
+	#include "../controller.h"
+
+	// --------------------------------------------------------------------
+
+	#define kb_led_num_on()      _kb_led_1_on()
+	#define kb_led_num_off()     _kb_led_1_off()
+	#define kb_led_caps_on()     _kb_led_2_on()
+	#define kb_led_caps_off()    _kb_led_2_off()
+	#define kb_led_scroll_on()   _kb_led_3_on()
+	#define kb_led_scroll_off()  _kb_led_3_off()
+
+	// --------------------------------------------------------------------
+
+	#include "./default--led-control.h"
+	#include "./default--matrix-control.h"
+
+#endif
+
